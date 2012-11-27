fd9ce1ab8d57808ff9a9bf95ee9c0a4cd6de4c8d
Ing. Jan Kaláb <pitlicek@gmail.com>
2012-11-27 09:22:34 +0100

    android create project

---
+++ src/cz/ipex/iptelefon/IpActivity.java
@@ -0,0 +1,15 @@
+package cz.ipex.iptelefon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IpActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
